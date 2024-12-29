51866fb403ed108eb28d9a94b364b94558c02064
Adrian Castro <22133246+castdrian@users.noreply.github.com>
2024-12-29 11:50:06 +0100

    chore: cleanup

--- OpenInDiscord Extension/Resources/settings.js
+++
@@ -1,9 +0,0 @@
-export const defaultSettings = {
-    showBanner: true,
-    instantOpen: false,
-    urlScheme: 'com.hammerandchisel.discord',
-    enableInvites: true,
-    enableUsers: true,
-    enableChannels: true,
-    enableFavorites: true
-}; 
